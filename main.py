fa5ed0166941094634d42b6567734017759f857c
natekimball <natekimball1@gmail.com>
2022-11-13 15:27:53 -0500

    moved files around

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ app = FastAPI()
 @app.post("/match")
 async def uploadfiles(file1: UploadFile = File(...), file2: UploadFile = File(...)):
     a, b = [x.decode("utf-8") for x in [await file1.read(), await file2.read()]]
-    f1, f2 = open("file1.txt", "w"), open("file2.txt", "w")
+    f1, f2 = open("input/file1.txt", "w"), open("input/file2.txt", "w")
     f1.writelines(a)
     f2.writelines(b)
     f1.close()
     f2.close()
-    f1,f2 = open("file1.txt", "r"), open("file2.txt", "r")
+    f1,f2 = open("input/file1.txt", "r"), open("input/file2.txt", "r")
     [result, _] = Matching.match(f1,f2)
     return result
