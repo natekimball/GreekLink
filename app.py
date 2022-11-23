d9828e91ac6884629befd8740b258447dda2ee7b
natekimball <natekimball1@gmail.com>
2022-11-23 00:49:06 -0500

    added get endpoints and edited dockerfile

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from mangum import Mangum
 
 app = FastAPI()
 
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to the Matchr"}
+
 @app.post("/big_little_match")
 async def big_little(littlefile: UploadFile = File(...), bigfile: UploadFile = File(...)):
     lf,bf = await handlefiles(littlefile, bigfile)
@@ -14,12 +18,20 @@ async def big_little(littlefile: UploadFile = File(...), bigfile: UploadFile = F
         "result": result
     }
 
+@app.get("/big_little_match")
+async def get_big_little():
+    return {"message": "Please use POST request to submit big and little files"}
+
 @app.post("/match")
-async def equal(file1: UploadFile = File(...), file2: UploadFile = File(...)):
+async def match(file1: UploadFile = File(...), file2: UploadFile = File(...)):
     f1,f2 = await handlefiles(file1,file2)
     # print(f1.readlines())
     # print(f2.readlines())
     return LinearMatching.match(f1,f2)
+
+@app.get("/match")
+async def get_match():
+    return {"message": "Please use POST request to submit files"}
 
 @app.post("/custom_match/{table}/{cols}/{rows}")
 async def custom_match(table, cols, rows, file1: UploadFile = File(...), file2: UploadFile = File(...)):
@@ -33,6 +45,10 @@ async def custom_match(table, cols, rows, file1: UploadFile = File(...), file2: 
             x+=1
     f1,f2 = await handlefiles(file1,file2)
     return LinearMatching.custom_match(f1,f2, table)
+
+@app.get("/custom_match")
+async def get_custom_match():
+    return {"message": "Please use POST request to submit files and custom table"}
 
 async def handlefiles(file1, file2):
     a, b = [x.decode("utf-8") for x in [await file1.read(), await file2.read()]]
